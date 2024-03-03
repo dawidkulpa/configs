b59a9b863e2802b6f3375331aa257c17a1ed2d09
Dawid Kulpa <kontakt@dkulpa.eu>
2024-03-03 01:22:58 +0000

    adds docker flake

---
+++ nix/modules/host/docker.nix
@@ -0,0 +1,23 @@
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+with lib; let
+  cfg = config.my.programs.dockerServer;
+in {
+  options.my.programs.dockerServer = {
+    enable = mkEnableOption "my docker server configuration";
+  };
+
+  config = mkIf cfg.enable {
+    virtualisation.docker = {
+      enable = true;
+      enableOnBoot = true;
+    };
+
+    networking.firewall.allowedTCPPorts = [];
+    networking.firewall.allowedUDPPorts = [];
+  };
+}
