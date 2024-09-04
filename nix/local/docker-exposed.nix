f2cca8d58d5afe4eb087bef8c20638e4059d4f13
Dawid Kulpa <kontakt@dkulpa.eu>
2024-09-04 15:43:40 +0000

    adds a service to cleanup docker-exposed memory

--- nix/local/docker-exposed.nix
+++ nix/local/docker-exposed.nix
@@ -17,6 +17,14 @@
     interval = "weekly";
   };
 
+  systemd.services.freeMemory = {
+    description = "nix optimise makes a big blueprint on memory, which doesn't work well with proxmox's discard, so this should free up the memory once a day";
+    after = "nix-optimise.service";
+    script = ''
+      sh -c "echo 1 > /proc/sys/vm/drop_caches"
+    '';
+  };
+
   swapDevices = [];
   networking.hostName = "docker-exposed";
 }
