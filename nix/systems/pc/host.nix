22ef2f3a448d474a543ef8de0c1490e91b64ec14
Dawid Kulpa <kontakt@dkulpa.eu>
2024-11-09 15:21:18 +0100

    updates pc flake

--- nix/systems/pc/host.nix
+++ nix/systems/pc/host.nix
@@ -10,8 +10,24 @@
   ];
 
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.enable = false;
+  boot.loader.efi.canTouchEfiVariables = false;
+  boot.loader.grub = {
+    enable = true;
+    device = "nodev";
+    useOSProber = true;
+    efiSupport = true;
+    efiInstallAsRemovable = true;
+    extraEntriesBeforeNixOS = true;
+    extraEntries = ''
+      menuentry "Poweroff" {
+        halt
+      }
+      menuentry "Reboot" {
+        reboot
+      }
+    '';
+  };
 
   networking = {
     hostName = "nixosPC";
