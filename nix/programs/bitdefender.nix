068f084a6d2de0151b183e1d7618d9a97ca7508b
Dawid Kulpa <kontakt@dkulpa.eu>
2025-05-14 20:23:22 +0200

    adds bitdefender

--- nix/programs/bitdefender.nix
+++ nix/programs/bitdefender.nix
@@ -1,6 +1,5 @@
 { lib, 
 pkgs, 
-stdenv, 
 config,
 dpkg, 
 fetchurl, 
@@ -26,31 +25,30 @@ in {
   };
 
   config = mkIf cfg.enable (let 
-    bitdefender = stdenv.mkDerivation rec {
+    bitdefender = pkgs.stdenv.mkDerivation rec {
       pname = "bitdefender-endpoint-security-tools";
       inherit version;
 
       openssl = pkgs.openssl_1_1; # Specify the correct version here
 
-      src = fetchurl {
+      src = pkgs.fetchurl {
         url = "file:///home/buggy/bitdefender/bitdefender-security-tools_${version}_amd64.deb";
         sha256 = "a8ba0225f2ee342e1f0ed41772eb0d53c8a3caffe419138fe0a2b029171a4a58";
       };
 
-      nativeBuildInputs = [
+      nativeBuildInputs = with pkgs; [
         dpkg
         makeWrapper
         autoPatchelfHook
       ];
 
-      buildInputs = [
-        stdenv.cc.cc.lib
+      buildInputs = with pkgs; [
         libxml2
-        openssl
+        openssl_1_1
         zlib
         gcc
         glibc
-        libcxx
+        llvmPackages.libcxx   # libcxx lives here
         libgcc
         audit
         libgcrypt
